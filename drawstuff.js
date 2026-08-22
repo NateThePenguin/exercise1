40d462372833e98a081cf0d47523dfce48b8f8b5
Nate <72577456+NateThePenguin@users.noreply.github.com>
2026-08-22 16:49:48 -0400

    putting the color back to teal

--- drawstuff.js
+++ drawstuff.js
@@ -78,7 +78,7 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Draw a rectangle with pixels
-    var c = new Color(0,250,150,255); // the color at the pixel: black opaque
+    var c = new Color(0,150,150,255); // the color at the pixel: black opaque
     for (var x=50; x<2000; x++) 
         for (var y=50; y<2750; y++) {
             drawPixel(imagedata,x,y,c);
